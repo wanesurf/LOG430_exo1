61e1de2639d906b9b2f746bf38c220bc6cedb756
L1 <helwan.mande.1@ens.etsmtl.ca>
2021-09-14 21:50:55 -0400

    cannot post to apache from ms

--- video-streaming/video-upload/index.js
+++ video-streaming/video-upload/index.js
@@ -14,12 +14,17 @@ const storage = multer.diskStorage({
         cb(null, '/usr/src/app')
     },
     filename: function (req, file, cb) {
-        cb(null, file.fieldname  + '.jpg')
+        cb(null, file.fieldname  + '.mp4')
     }
 })
 
 const upload = multer({ storage: storage })
+// const upload = multer({ dest: 'http://localhost:4001' })
+
 const app = express();
+
+const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
+const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
 
 function main () {
 
@@ -29,13 +34,57 @@ function main () {
     
     })
 
-    app.post('/upload', upload.single('avatar'), function (req, res, next) {
+    // saving locally
+    app.post('/save', upload.single('avatar'), function (req, res, next) {
         // req.file is the `avatar` file
         console.log(req.file)
-        res.send("fichier uploader vers le micro service apache")
+        // res.send("fichier uploader vers le micro service apache")
+        // res.send(`You have uploaded this image: <hr/><img src="${req.file.path}" width="500"><hr /><a href="./">Upload another image</a>`);
 
         // req.body will hold the text fields, if there were any
     })
+
+    //saving to apache micro service
+     app.post('/upload', upload.single('avatar'), function (req, res, next) {
+         console.log(req.file)
+
+
+         const postData = JSON.stringify({
+             'msg': 'Hello World!'
+         });
+
+         const options = {
+             hostname: VIDEO_STORAGE_HOST,
+             port: VIDEO_STORAGE_PORT,
+             path: '/',
+             method: 'POST',
+             headers: {
+                 'Content-Type': 'application/json',
+                 'Content-Length': Buffer.byteLength(postData)
+             }
+         };
+
+         const forward = http.request(options, (res) => {
+             console.log(`STATUS: ${res.statusCode}`);
+             console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+             res.setEncoding('utf8');
+             res.on('data', (chunk) => {
+                 console.log(`BODY: ${chunk}`);
+             });
+             res.on('end', () => {
+                 console.log('No more data in response.');
+             });
+         });
+
+         forward.on('error', (e) => {
+             console.error(`problem with request: ${e.message}`);
+         });
+
+// Write data to request body
+         forward.write(postData);
+         forward.end();
+})
+
 
     app.listen(3000, ()=> {
 
